cd72a78b3f3da7f702859a9be6e9a035a3c541f2
AllanDav1d <allan.j.k15@gmail.com>
2021-07-21 15:39:13 -0300

    Chamadas HTTP

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,19 +1,27 @@
+import { useEffect, useState } from "react";
 import RepositoryItem from "./RepositoryItem";
 
-const repository = {
-  name: 'teste',
-  description: 'Forms in react',
-  link: 'https://google.com/'
-}
+import '../styles/repositories.scss';
 
 export default function RepositoryList() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
+
       </ul>
     </section>
   )
